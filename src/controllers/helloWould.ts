6d1bfeca15925f7d05ebb2b5e9fd64512481facb
thaithuytb <ngoquangthai29112001@gmail.com>
2022-08-18 18:04:08 +0700

    fix test to re-run pull request

--- src/controllers/helloWould.ts
+++ src/controllers/helloWould.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 const helloWould = (req: Request, res: Response) => {
   res.status(200);
-  res.json('Hello would !!!');
+  res.json('Hello would !!! -- aftertest deploy github action !');
 };
 
 export default helloWould;
