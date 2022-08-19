363ad8981f228f8fdb1b7c12ff924c6e5363c7ea
thaithuytb <ngoquangthai29112001@gmail.com>
2022-08-19 13:32:14 +0700

    connect server to mysql

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+
 import helloWould from './controllers/helloWould';
 import TodoController from './controllers/todo.controller';
+import connectDB from './database/connect/index';
 
+dotenv.config();
 const app = express();
+
+connectDB.connect((err) => {
+  if (err) throw err;
+  console.log('Database is connected !!!');
+});
 app.use(
   cors({
     origin: 'http://localhost:3000',
--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,28 +1,17 @@
 import { Request, Response } from 'express';
+import { FieldInfo, MysqlError } from 'mysql';
+import connectDB from '../database/connect/index';
 interface Todo {
-    id: number;
-    name: string;
-    complete: boolean;
+  id: number;
+  name: string;
+  complete: boolean;
 }
+
 export default class TodoController {
   static getTodos(req: Request, res: Response) {
-    const todos: Todo[] = [
-      {
-        id: 1,
-        name: 'run',
-        complete: false,
-      },
-      {
-        id: 2,
-        name: 'eat',
-        complete: false,
-      },
-      {
-        id: 2,
-        name: 'fly',
-        complete: false,
-      },
-    ];
-    return res.status(200).json(todos);
+    connectDB.query('SELECT * FROM todos', (err: MysqlError, rows: any) => {
+      if (err) throw err;
+      return res.status(200).json([...rows]);
+    });
   }
 }
